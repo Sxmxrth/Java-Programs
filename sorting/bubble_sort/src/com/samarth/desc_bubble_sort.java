93253c771247190f7f5248e2860e2487da6c2f02
Samarth Gupta <samarthgupta1202@gmail.com>
2022-02-27 11:20:36 +0530

    certain files added and updated

---
+++ sorting/bubble_sort/src/com/samarth/desc_bubble_sort.java
@@ -0,0 +1,29 @@
+package com.samarth;
+
+import java.util.Arrays;
+
+public class desc_bubble_sort {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        bubble_sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble_sort(int[] arr){
+        boolean swapped;
+        for (int i = arr.length-1; i >0 ; i--) {
+            swapped=false;
+            for (int j = 0; j <i ; j++) {
+                if(arr[j]<arr[j+1]){
+                    arr[j]=arr[j]+arr[j+1];
+                    arr[j+1]=arr[j]-arr[j+1];
+                    arr[j]=arr[j]-arr[j+1];
+                    swapped=true;
+                }
+            }
+            if (!swapped){
+                break;
+            }
+        }
+
+    }
+}
