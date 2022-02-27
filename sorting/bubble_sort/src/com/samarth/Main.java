15c231ac385eea69f7ad738fa07c3c74310ae939
Samarth Gupta <samarthgupta1202@gmail.com>
2022-02-27 11:14:43 +0530

    optimised bubble sort

--- sorting/bubble_sort/src/com/samarth/Main.java
+++ sorting/bubble_sort/src/com/samarth/Main.java
@@ -5,22 +5,27 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr={3,2,5,4,1};
-        sort(arr);
+        int[] arr={1,2,3,4,5};
+        bubble_sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    static void sort(int[] arr){
+    static void bubble_sort(int[] arr){
+        boolean swapped;
         for (int i = arr.length-1; i >0 ; i--) {
+            swapped=false;
             for (int j = 0; j <i ; j++) {
                 if(arr[j]>arr[j+1]){
                     arr[j]=arr[j]+arr[j+1];
                     arr[j+1]=arr[j]-arr[j+1];
                     arr[j]=arr[j]-arr[j+1];
+                    swapped=true;
                 }
-
             }
-
+            if (!swapped){
+                break;
+            }
         }
-        System.out.println(Arrays.toString(arr));
+
     }
 }
