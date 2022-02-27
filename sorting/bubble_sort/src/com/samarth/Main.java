06ee62fd07ebe42ca4d659db41c1f21d211e0427
Samarth Gupta <samarthgupta1202@gmail.com>
2022-02-27 10:51:03 +0530

    bubble sort updated

--- sorting/bubble_sort/src/com/samarth/Main.java
+++ sorting/bubble_sort/src/com/samarth/Main.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr={45,67,1,2,0,9,23};
+        int[] arr={34,67,90,1,3,2,100};
         sort(arr);
     }
 
     static void sort(int[] arr){
-        for (int i = 1; i <arr.length ; i++) {
-            for (int j = 0; j <arr.length-1 ; j++) {
+        for (int i = arr.length-1; i >0 ; i--) {
+            for (int j = 0; j <i ; j++) {
                 if(arr[j]>arr[j+1]){
                     arr[j]=arr[j]+arr[j+1];
                     arr[j+1]=arr[j]-arr[j+1];
