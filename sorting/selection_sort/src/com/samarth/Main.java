9ca24b7e8e4dc6ac07387ed10f37d8d1777c218b
Samarth Gupta <samarthgupta1202@gmail.com>
2022-03-06 15:23:12 +0530

    files added and updated

--- sorting/selection_sort/src/com/samarth/Main.java
+++ sorting/selection_sort/src/com/samarth/Main.java
@@ -5,26 +5,26 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	    int[] arr={3,2,5,4,1};
+	    int[] arr={1,2,-9,-6,90};
         selection_sort(arr);
 
     }
     static void selection_sort(int[] arr){
 
-        for (int i = 0; i <arr.length ; i++) {
-            int max=0;
-            int last=arr.length-i-1;
-            for (int j = 1; j <=last ; j++) {
-
-                if(arr[max]<arr[j]){
-                    max=j;
+        for (int i =arr.length-1; i >0 ; i--) {
+            int max=arr[0],k=0;
+            for (int j = 0; j <=i ; j++) {
+                if(arr[j]>max){
+                    max=arr[j];
+                    k=j;
                 }
-
             }
-
-
+            int t=arr[k];
+            arr[k]=arr[i];
+            arr[i]=t;
 
         }
         System.out.println(Arrays.toString(arr));
+
     }
 }
